46c060518d2234c38b0fdb81a63201c16e5687d2
Khalil Elkharbibi <140761974+atlas-lion91@users.noreply.github.com>
2023-10-17 18:22:07 -0400

    Update running_processes.py

--- running_processes.py
+++ running_processes.py
@@ -10,9 +10,15 @@ for process in psutil.process_iter():
         process_info = process.as_dict(attrs=['pid', 'name', 'exe', 'cpu_percent', 'memory_percent'])
         
         # Append to the list of all processes
-        all_processes.append(process_info)
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        pass
+       all_processes.append(process_info)
+    except psutil.NoSuchProcess:
+        print(f"Error: Process with PID {process_info['pid']} does not exist anymore.")
+    except psutil.AccessDenied:
+        print(f"Error: Access denied to fetch details for process with PID {process_info['pid']}.")
+    except psutil.ZombieProcess:
+        print(f"Error: Process with PID {process_info['pid']} is a zombie process.")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
 with open('process.csv', 'w', newline='') as csvfile:
     fieldnames = ['pid', 'name', 'exe', 'cpu_percent', 'memory_percent']
