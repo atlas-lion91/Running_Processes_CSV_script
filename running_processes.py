3d29b0e253538418a2b5ad210460962c1987f969
Khalil Elkharbibi <140761974+atlas-lion91@users.noreply.github.com>
2023-10-17 18:27:53 -0400

    Update running_processes.py

--- running_processes.py
+++ running_processes.py
@@ -2,15 +2,17 @@ import os
 import psutil
 import csv
 
-# Retreive all running processes
+# Create a list to store process details
 all_processes = []
+
+# Retrieve details of all running processes
 for process in psutil.process_iter():
     try:
         # Retrieve process details as a named tuple
         process_info = process.as_dict(attrs=['pid', 'name', 'exe', 'cpu_percent', 'memory_percent'])
         
         # Append to the list of all processes
-       all_processes.append(process_info)
+        all_processes.append(process_info)
     except psutil.NoSuchProcess:
         print(f"Error: Process with PID {process_info['pid']} does not exist anymore.")
     except psutil.AccessDenied:
@@ -20,13 +22,20 @@ for process in psutil.process_iter():
     except Exception as e:
         print(f"Unexpected error: {e}")
 
-with open('process.csv', 'w', newline='') as csvfile:
-    fieldnames = ['pid', 'name', 'exe', 'cpu_percent', 'memory_percent']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+# Check if we have any processes to write to CSV
+if not all_processes:
+    print("No process details were fetched. Exiting.")
+    exit()
 
-
-    writer.writeheader()
-    for process in all_processes:
-        writer.writerow(process)
-
-print("CSV file created successfully!")
+# Write processes to a CSV file
+try:
+    with open('processes.csv', 'w', newline='') as csvfile:
+        fieldnames = ['pid', 'name', 'exe', 'cpu_percent', 'memory_percent']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        for process in all_processes:
+            writer.writerow(process)
+    print("CSV file created successfully!")
+except Exception as e:
+    print(f"Error writing to CSV file: {e}")
